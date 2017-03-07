Functional generator and encoder. May have some problems regarding exploding gradients. Added a fix testing now whether it is working.

step_one/IO.py
@@ -19,7 +19,7 @@ import numpy as np
 import sys
 import gzip
 import random
-
+import json
 
 #####################
 # Code from Tau lei #
@@ -96,12 +96,13 @@ def read_annotations(path):
             y = np.asarray([ float(v) for v in y ])
             data_x.append(x)
             data_y.append(y)
-    say("{} examples loaded from {}\n".format(
+    
+    print "{} examples loaded from {}\n".format(
             len(data_x), path
-        ))
-    say("max text length: {}\n".format(
+        )
+    print "max text length: {}\n".format(
         max(len(x) for x in data_x)
-    ))
+    )
     return data_x, data_y
     
     
@@ -284,3 +285,14 @@ def create_embedding_layer(path):
             fix_init_embs = False
         )
     return embedding_layer
+    
+def read_rationales(path):
+    data = [ ]
+    fopen = gzip.open if path.endswith(".gz") else open
+    with fopen(path) as fin:
+        for line in fin:
+            item = json.loads(line)
+            data.append(item)
+    return data
+
+

step_one/advanced_layers.py
@@ -45,7 +45,7 @@ class RCNNCell(RNNCell):
                                     [n_in, n_out],initializer = tf.random_uniform_initializer(-0.05, 0.05)                                  # Dims 
                                     )
             
-            tf.histogram_summary(scope or name + 'weights', W)
+            #tf.histogram_summary(scope or name + 'weights', W)
 
             
             out = tf.matmul(inputs, W)
@@ -392,9 +392,9 @@ class Z_Layer(object):
             w1 = tf.get_variable('W1', dtype = tf.float32)
             w2 = tf.get_variable('W2', dtype = tf.float32)
             bias = tf.get_variable('Bias', dtype = tf.float32)
-            self.w1sum = tf.histogram_summary('ZlayerW1', w1)
-            self.w2sum = tf.histogram_summary('ZlayerW2', w2)
-            self.biassum = tf.histogram_summary('ZlayerB', bias)
+            #self.w1sum = tf.histogram_summary('ZlayerW1', w1)
+            #self.w2sum = tf.histogram_summary('ZlayerW2', w2)
+            #self.biassum = tf.histogram_summary('ZlayerB', bias)
 
         
         # z would be ( len, batch_size)

step_one/optimization_updates.py
@@ -21,12 +21,26 @@ def create_optimization_updates(cost, params = None, method = 'sgd',
     
     if method == 'sgd':
         opt = tf.train.GradientDescentOptimizer(learning_rate = lr).minimize(cost)
+        gnorms = None
     elif method == 'adadelta':
         opt = tf.train.AdadeltaOptimizer(learning_rate = lr, rho = rho).mimimze(cost)
+        gnorms = None
     elif method == 'adagrad':
         opt = tf.train.AdagradOptimizer(learning_rate = lr).minimize(cost)
+        gnorms = None
     elif method =='adam':
-        opt = tf.train.AdamOptimizer(learning_rate = lr, beta1 = beta1, beta2= beta2).minimize(cost)
-    
-    return opt
         
+        # opt = tf.train.AdamOptimizer(learning_rate = lr, beta1 = beta1, beta2= beta2).minimize(cost)
+        opt_uncl = tf.train.AdamOptimizer(learning_rate = lr,
+                                          beta1 = beta1,
+                                          beta2= beta2)
+        
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), 1)
+        opt = opt_uncl.apply_gradients(zip(grads, tvars))
+        
+        gnorms = None
+
+        
+    
+    return opt, gnorms