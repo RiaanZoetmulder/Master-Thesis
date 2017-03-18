Fixed the numirical overflow.
Works!

Next update: Cleaned up code!

step_one/advanced_layers.py
@@ -18,10 +18,30 @@ from tensorflow.python.ops.math_ops import tanh
 from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.ops import array_ops
 from tensorflow.python.util import nest
+from basic_layers import BasicRNNCell, _linear
+
+tf.set_random_seed(2345)
 
 ###############################
 #######    RCNNCell   #########
 ###############################
+
+def ln(tensor, scope = None, epsilon = 1e-5):
+    """ Layer normalizes a 2D tensor along its second axis """
+    assert(len(tensor.get_shape()) == 2)
+    m, v = tf.nn.moments(tensor, [1], keep_dims=True)
+    if not isinstance(scope, str):
+        scope = ''
+    with tf.variable_scope(scope + 'layer_norm'):
+        scale = tf.get_variable('scale',
+                                shape=[tensor.get_shape()[1]],
+                                initializer=tf.constant_initializer(1))
+        shift = tf.get_variable('shift',
+                                shape=[tensor.get_shape()[1]],
+                                initializer=tf.constant_initializer(0))
+    LN_initial = (tensor - m) / tf.sqrt(v + epsilon)
+
+    return LN_initial * scale + shift
 
 class RCNNCell(RNNCell):
     '''
@@ -42,7 +62,7 @@ class RCNNCell(RNNCell):
         with vs.variable_scope(scope or name, reuse = reuse) as scop:
             
             W = tf.get_variable('weights' + str(order) ,                            # naming
-                                    [n_in, n_out],initializer = tf.random_uniform_initializer(-0.05, 0.05)                                  # Dims 
+                                    [n_in, n_out],initializer = tf.random_uniform_initializer(-0.05, 0.05, seed = 2345)                                  # Dims 
                                     )
             
             #tf.histogram_summary(scope or name + 'weights', W)
@@ -69,9 +89,11 @@ class RCNNCell(RNNCell):
                  activation = tanh, 
                  mode = 1,
                  has_outgate= False,
-                 state_size = 100,
-                 idx = 1             # custom  id, 1 by default
+                 state_size = 100,      
+                 idx = 1,              # custom  id, 1 by default
+                 use_ln = False        # use layer normalization
                  ):
+        
         '''
         RCNN
         Inputs
@@ -102,6 +124,7 @@ class RCNNCell(RNNCell):
         self._has_outgate = has_outgate
         
         self.reuse = None
+        self.useln = use_ln
 
     @property
     def state_size(self):
@@ -157,7 +180,7 @@ class RCNNCell(RNNCell):
                             [self._num_units,],
                             initializer = tf.constant_initializer(0.0),  dtype = tf.float32)
             
-            forget_cell = tf.nn.rnn_cell.BasicRNNCell(self._num_units, activation = tf.nn.sigmoid)
+            forget_cell = BasicRNNCell(self._num_units, activation = tf.nn.sigmoid)
             forget_t = forget_cell(inputs, ht_m1)[0]
             
         lst = [ ]   
@@ -190,19 +213,27 @@ class RCNNCell(RNNCell):
             
             else:
                 c_i_t = forget_t * c_i_tm1 + (1.0-forget_t) * (in_i_t + c_im1_tm1)
-                
-            lst.append(c_i_t)
+            
+            # whether to use layer normalization
+            if self.useln:
+                lst.append(ln(c_i_t, scope = 'cit' + str(self._idx) + str(i)+ '/'))
+            else:
+                lst.append(c_i_t)
+            
             c_im1_tm1 = c_i_tm1
             c_im1_t = c_i_t
         
         
         # Use an outgate or not!
         if not self._has_outgate:
-            h_t = self._activation(c_i_t + bias, name = 'no_outgate')
+            if self.useln:
+                h_t = ln(self._activation(c_i_t + bias, name = 'no_outgate'), scope = 'ht_' + str(self._idx))
+            else:
+                h_t = self._activation(c_i_t + bias, name = 'no_outgate')
             
         else:
             with vs.variable_scope(scope or self.name, reuse = self.reuse ) as var_scope:
-                out_t = tf.nn.rnn_cell._linear([inputs, ht_m1],
+                out_t = _linear([inputs, ht_m1],
                                                  self._num_units,
                                                 True, 1.0,
                                                  scope = scope or "RCNN_cell" + '_%s'% (str(self._idx) + 'out_t'))
@@ -313,6 +344,28 @@ class MultiRNNCell(RNNCell):
 ###############################
 #######    Z-Layer    #########
 ###############################
+def stable_sigmoid(x):
+    '''
+    Numerically stable sigmoid function
+    '''
+    
+    print 'xshape: ', x.get_shape()
+    x = tf.squeeze(x, squeeze_dims= [2])
+    print 'xshape squeeze: ', x.get_shape()
+    def softmax(x):
+        return tf.ones_like(x, tf.float32) /(tf.ones_like(x, tf.float32)+ tf.exp(-x))
+        
+    def softplus(x):
+        return tf.exp(x)/(tf.ones_like(x, tf.float32) + tf.exp(x))
+    
+    sftmx = softmax(x)
+    sftpls = softplus(x)
+    
+    condition = tf.greater_equal(x, tf.zeros_like(x, tf.float32))
+    print 'shape condition:', condition.get_shape()
+    val = tf.select(condition, sftmx, sftpls)
+    print 'shape val:', val.get_shape()
+    return tf.expand_dims(val, 2)
 
 class Z_Layer(object):
     
@@ -340,9 +393,9 @@ class Z_Layer(object):
         
         with vs.variable_scope('ZLayerWeights') as var_scope: 
             w1 = tf.get_variable('W1', [n_in,1], dtype = tf.float32, 
-                                 initializer = tf.random_uniform_initializer(-0.05, 0.05))
+                                 initializer = tf.random_uniform_initializer(-0.05, 0.05, seed = 2345))
             w2 = tf.get_variable('W2', [n_hidden,1], dtype = tf.float32, 
-                                 initializer = tf.random_uniform_initializer(-0.05, 0.05))
+                                 initializer = tf.random_uniform_initializer(-0.05, 0.05, seed = 2345))
             bias = tf.get_variable('Bias', 
                                    [1], 
                                    initializer=tf.constant_initializer(0.0), 
@@ -374,12 +427,15 @@ class Z_Layer(object):
                     tf.matmul(h_tm1[:,-self.n_hidden:], w2) +
                     bias, name = 'pzt_sigmoid'
                 )
+        logits = tf.matmul(x_t, w1) + \
+                    tf.matmul(h_tm1[:,-self.n_hidden:], w2) +\
+                    bias
         
         xz_t =  tf.concat(1, [x_t, tf.reshape(z_t, [-1,1])], name = 'xzt_concat')
         
         rnn_outputs, final_state = self.rlayer( xz_t,  h_tm1)
         
-        return final_state, pz_t
+        return final_state, pz_t, logits
     
     
     def forward_all(self, x, z):
@@ -415,7 +471,12 @@ class Z_Layer(object):
             
             # here too changed the dynamic rnn to scan
             htp = tf.scan(self.rlayer, xz, initializer= h_temp)
-            h = htp[:,:, self.rlayer._order * self.rlayer._num_units:]
+            if len(htp.get_shape())>1:
+            
+                h = htp[:,:, self.rlayer._order * self.rlayer._num_units:]
+            else:
+                h = htp[:, self.rlayer._order * self.rlayer._num_units:]
+                
         
         # print 'rnn_outputs shape: ', rnn_outputs.get_shape()
         #print 'h shape: ', h.get_shape()
@@ -444,19 +505,26 @@ class Z_Layer(object):
         b = tf.reshape(b_tp, [-1, hshape[1], 1])
         
         # sigmoid it
-        pz = sigmoid(a+b+bias)
+        logits = a+b+bias
+        #pz = sigmoid(logits)
+        
+        pz = sigmoid(logits)
+        print 'shape pz: ', pz.get_shape()
         
         
         pz_reshaped = tf.squeeze(pz, squeeze_dims= [2])
+        logits_reshaped = tf.squeeze(logits, squeeze_dims= [2])
         
         # return the matrix of predictions
         assert len(pz_reshaped.get_shape()) == 2
+        assert len(logits_reshaped.get_shape()) == 2
+        print 'logits_reshaped: ', logits_reshaped.get_shape() 
         
-        return pz_reshaped
+        return pz_reshaped, logits_reshaped
     
         
     # adapted version of sample
-    def sample(self, prev, x_t, seed = 214):
+    def sample(self, prev, x_t, seed = 2345):
         
         # get the appropriate values
         h_tm1 = prev[0]
@@ -479,8 +547,10 @@ class Z_Layer(object):
         pz_t = tf.squeeze(pz_t)
         
         # TODO: Changed less equal into larger equal change back if fail
-        z_t = tf.cast(tf.less_equal(tf.random_uniform(pz_t.get_shape(), dtype=tf.float32, seed=seed), pz_t),
-                      tf.float32)
+        z_t = tf.cast(tf.less_equal(tf.random_uniform(pz_t.get_shape(),
+                                                      dtype=tf.float32, seed=seed),
+                                                        pz_t),
+                                                      tf.float32)
         
         xz_t = tf.concat(1,[x_t, tf.reshape(z_t,  [-1,1])])
         
@@ -531,7 +601,7 @@ class ExtRCNNCell(RCNNCell):
         x_t, mask_t = x[0], x[1]
         hc_t  = super(ExtRCNNCell, self).__call__(hc_tm1, x_t)
         a= mask_t * hc_t 
-        b = (1.0-mask_t) * hc_tm1    
+        b = (1 - mask_t) * hc_tm1    
         hc_t = a + b
         return hc_t
 

step_one/models.py
@@ -17,7 +17,7 @@ import json
 ###############################
 #######    Generator  #########
 ###############################
-
+tf.set_random_seed(2345)
 class Generator(object):
     
     
@@ -36,7 +36,7 @@ class Generator(object):
     def ready(self):
         args = self.args
         # inputs for feed dict.
-        # x should be a matrix of word Id's, integer valued
+        # x should be a matrix of word Id's, integer valued self.args.max_len
         # embedding placeholder is 
         self.x = x = tf.placeholder(tf.int64, [None, self.args.max_len], name='input_placeholder')  # None, 256
         self.embedding_placeholder = embedding_placeholder = tf.placeholder(tf.float32, 
@@ -142,25 +142,29 @@ class Generator(object):
                 # sample a which words should be kept
                 zpred = output_layer.sample_all(h_final)
                 
+                
                 self.zpredsum = tf.reduce_sum(zpred)
+                
                 # z itself should not be updated
                 zpred = tf.stop_gradient(zpred)
         
                 # get the probabilities and log loss
                 with tf.name_scope('zlayer_forward_pass'):
-                    probs = self.probs= output_layer.forward_all(h_concat, zpred)
+                    probs, logits = output_layer.forward_all(h_concat, zpred)
                 
                 with tf.name_scope('sigmoid_cross_entropy'):
-                    logpz  = (zpred*tf.log(probs) + (1.0 - zpred)* tf.log(1.0 - probs)) * masks
+                    logpz = (-logits*(1-zpred)-tf.nn.softplus(-logits)) * masks
+                    #logpz  = (zpred*tf.log(probs + 1e-6) + (1.0 - zpred)* tf.log(1.0 - probs + 1e-6)) * masks
+                    #logpz = -tf.nn.sigmoid_cross_entropy_with_logits(zpred, logits) * masks
                 
                 logpz = self.logpz = tf.reshape(logpz,tf.shape(x), name = 'reshape_logpz')
                 probs = self.probs = tf.reshape(probs, tf.shape(x), name = 'probs_reshape')
                 
                 # assign z
                 z = self.zpred = zpred
+                self.ztotsum = tf.reduce_sum(zpred)
             
                 # sum z
-                
                 with tf.name_scope('operations_on_z'):
                     self.zsum = tf.reduce_sum(z, 0, name = 'zsum')
                     self.zdiff = tf.reduce_sum(tf.abs(z[1:]-z[:-1]),  0, name = 'zdiff')
@@ -174,7 +178,16 @@ class Generator(object):
                 for dim in sh:
                     variable_parametes *= dim.value
                 total_parameters += variable_parametes
+            
             print 'total #  Generator parameters:', total_parameters
+            
+            print 'trainable variables: \n'
+            for x in tf.trainable_variables():
+                print x.name
+            
+            string= 'Generator/RCNN_Feed_Forward_Layer_0_0/weights0:0'
+            self.weights = [v for v in tf.trainable_variables() if v.name == string][0]
+            
             
             # get l2 cost for all parameters
             varls = tf.trainable_variables() 
@@ -207,11 +220,13 @@ class Encoder(object):
             # variables from the generator
             dropout = gen.dropout
             x = gen.x
+            
             # removed z here. can add back if you want to
             z = tf.expand_dims(gen.zpred, 2)
 
             # input placeholder
-            y = self.y = tf.placeholder(tf.float32, [None, self.nclasses], name= 'target_values')
+            y = self.y = tf.placeholder(tf.float32, [None, self.nclasses],
+                                        name= 'target_values')
 
             n_d = args.hidden_dimension
             n_e = emb_layer.n_d
@@ -318,13 +333,17 @@ class Encoder(object):
     
                     loss_vec = loss_mat[:,args.aspect]
     
-                self.loss_vec = loss_vec
-    
+                # self.loss_vec = loss_vec
+                self.loss_vec = tf.check_numerics(loss_vec, 'loss_vec has Nan or Inf values')
+                
                 # get values from the generator
-                zsum = gen.zsum
-                zdiff = gen.zdiff
-                logpz = gen.logpz
-    
+                self.zsum = zsum = gen.zsum
+                self.zdiff = zdiff = gen.zdiff
+                self.logpz = logpz = gen.logpz
+                
+                zsum = tf.check_numerics(zsum, 'Zsum has Nan or Inf values')
+                zdiff = tf.check_numerics(zdiff, 'Zdiff has Nan or Inf values')
+                logpz = tf.check_numerics(logpz, 'logpz has Nan or Inf values')
     
                 coherent_factor = args.sparsity * args.coherent
                 loss = self.loss = tf.reduce_mean(loss_vec)
@@ -386,6 +405,8 @@ class Model(object):
         self.args = args
         self.embedding_layer = embedding_layer
         self.nclasses = nclasses
+        
+        self.obj_array = []
 
 
     def ready(self):
@@ -402,6 +423,7 @@ class Model(object):
         
     
     def train(self, train, dev, test, rationale_data, sess):
+        
         '''
         Function to do training procedure, use args to set which optimizer
         and what parameters.
@@ -512,8 +534,9 @@ class Model(object):
                 for i in xrange(N):
                     
                     # notify user for elapsed time
-                    if (i+1)%10 == 0:
+                    if (i+1)%50 == 0:
                         print "\r{}/{} {:.5f}       ".format(i+1,N,p1/(i+1))
+                        print 'ztotsum = ', ztotsum
                         
                     # training batches for this round
                     bx, by = train_batches_x[i], train_batches_y[i]
@@ -521,8 +544,8 @@ class Model(object):
                     mask = (bx != padding_id)
                     
                     if bx.shape[1] != 256:
-                        print 'shape of x: ', bx.shape
-                        continue 
+                        bx = bx.T
+                        continue
                     
     
                     
@@ -534,27 +557,27 @@ class Model(object):
                                  self.generator.lr: self.args.learning_rate}
                                  
                     #losse, lossg, logpz, masks, probs, avg, inp_avg, zpredsum= sess.run([self.encoder.loss_l2_e, self.encoder.loss_l2_g, self.generator.logpz, self.generator.masks, self.generator.probs, self.generator.avg, self.generator.avg_inp, self.generator.zpredsum], feed_dict)
-               
-                    
-                    
-                    _,_, cost, loss, sparsity_cost, bz, summary  = sess.run([train_step_enc,train_step_gen,
+                    _,_,  cost, loss, sparsity_cost, bz, summary, ztotsum  = sess.run([train_step_enc, train_step_gen,
                                                 self.encoder.obj,
                                                 self.encoder.loss,
                                                 self.encoder.sparsity_cost,
-                                                self.z, merged], 
+                                                self.z, merged, self.generator.ztotsum], 
                                                 feed_dict)
-                        
-
                     
-                    
-                    
+                    #print 'zpredsum = ', ztotsum
                     
                     k = len(by)
                     processed += k
                     train_cost += cost
                     train_loss += loss
                     train_sparsity_cost += sparsity_cost
-                    p1 += np.sum(bz*mask) / (np.sum(mask)+1e-8)
+                    temp = np.sum(bz*mask) / (np.sum(mask)+1e-8)
+                    p1 += temp
+                    
+                    if temp ==0 or p1 ==0:
+                        print 'p1 zero in training loop'
+                    
+                    
                     
                     
                 train_writer.add_summary(summary, epoch)
@@ -567,10 +590,13 @@ class Model(object):
                 if dev:
                     # development set stuff
                     dev_obj, dev_loss, dev_diff, dev_p1 = self.evaluate_data(
-                            dev_batches_x, dev_batches_y, sess, epoch, merged, eval_writer)
+                            dev_batches_x, dev_batches_y, sess, epoch, merged,
+                            eval_writer)
+                    
                     cur_dev_avg_cost = dev_obj
                     
                 if args.decay_lr and last_train_avg_cost is not None:
+                    
                     # report on training cost and development cost
                     if cur_train_avg_cost > last_train_avg_cost*(1+tolerance):
                         more = True
@@ -603,13 +629,17 @@ class Model(object):
                         (time.time()-start_time)/60.0/(i+1)*N
                     )
 
-                    
+                self.obj_array.append(train_loss / N)
+                
+                print 'Obj_array: \n'
+                print self.obj_array
                     
                 if dev:
                     if dev_obj < best_dev:
                         best_dev = dev_obj
                         unchanged = 0
                         if args.dump and rationale_data:
+                            
                             # implement dump rationales
                             self.dump_rationales(args.dump, valid_batches_x, valid_batches_y,
                                         sess)
@@ -661,18 +691,20 @@ class Model(object):
                          self.generator.lr:self.args.learning_rate
                          }
                
-            mask = bx != padding_id
+            mask = (bx != padding_id)
             
             
-            bz, o, e, d, summary = sess.run([self.z, self.encoder.obj,
-                                    self.encoder.loss, self.encoder.pred_diff, self.merged],
+            bz, o, e, d= sess.run([self.z, self.encoder.obj,
+                                    self.encoder.loss, self.encoder.pred_diff],
                                     feed_dict = feed_dict)
             p1 += np.sum(bz*mask) / (np.sum(mask) + 1e-8)
             tot_obj += o
             tot_mse += e
             tot_diff += d
             
-        eval_writer.add_summary(summary, epoch)
+        # eval_writer.add_summary(summary, epoch)
+        if p1 == 0:
+            print 'p1 zero in evaluate data'
         
         n = len(batches_x)
         return tot_obj/n, tot_mse/n, tot_diff/n, p1/n
@@ -759,6 +791,10 @@ class Model(object):
            
             tot_mse += e
             p1 += np.sum(bz*mask)/(np.sum(mask) + 1e-8)
+            
+            if p1 ==0:
+                print 'p1 zero in rationale loop'
+                raise NotImplementedError
             if args.aspect >= 0:
                 
                 for z,m in zip(bz.T, mask.T):

step_one/optimization_updates.py
@@ -30,14 +30,22 @@ def create_optimization_updates(cost, params = None, method = 'sgd',
         gnorms = None
     elif method =='adam':
         
-        # opt = tf.train.AdamOptimizer(learning_rate = lr, beta1 = beta1, beta2= beta2).minimize(cost)
+        #opt_uncl = tf.train.AdamOptimizer(learning_rate = lr, beta1 = beta1, beta2= beta2)
+        
         opt_uncl = tf.train.AdamOptimizer(learning_rate = lr,
-                                          beta1 = beta1,
-                                          beta2= beta2)
+                                         beta1 = beta1,
+                                         beta2= beta2).minimize(cost)
+        
+        
+        
+        opt = opt_uncl
+        '''
         
         tvars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), 1)
+        grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), 5)
         opt = opt_uncl.apply_gradients(zip(grads, tvars))
+        '''
+        
         
         gnorms = None
 

step_one/rationale_dependent.py
@@ -7,6 +7,7 @@ rationale_dependent.py
 from options import load_arguments
 from IO import create_embedding_layer, read_annotations, create_batches, read_rationales
 import tensorflow as tf
+import numpy as np
 
 from models import Model
 def main():
@@ -40,8 +41,11 @@ def main():
             
 
     if args.train:
-        with tf.Graph().as_default() as g: 
+        with tf.Graph().as_default() as g:
+            
+            # used to be set to 2345
             tf.set_random_seed(2345)
+            np.random.seed(2345)
             with tf.Session() as sess:
                 # initialize Model
                 #TODO: create encoder class in model
@@ -53,6 +57,7 @@ def main():
                         )
                 model.ready()
                 
+                
                 # added this for testing
                 # TODO: Remove later
                 model.train((train_x, train_y),
@@ -60,6 +65,8 @@ def main():
                             None,
                             rationale_data if args.load_rationale else None,
                             sess) 
+                
+                
             
             '''
             train_batches_x, train_batches_y = create_batches(
@@ -67,7 +74,7 @@ def main():
                             )
             
             feed_dict={model.generator.x: train_batches_x[0],model.encoder.y : train_batches_y[0], model.generator.embedding_placeholder: embed_layer.params[0], 
-                      model.generator.dropout: 0.5, model.generator.training: True}
+                      model.generator.dropout: 0.9, model.generator.training: True}
                       
             
             init = tf.initialize_all_variables()
@@ -77,9 +84,13 @@ def main():
             print 'past graph initialization'
             
             
-            # cost_g = sess.run(model.encoder.cost_g , feed_dict)
+            probs, logpz, ztotsum, weights = sess.run([model.generator.probs, model.generator.logpz, model.generator.ztotsum, model.generator.weights], feed_dict)
             
-            print cost_g
+            print 'probs: ', probs
+            print 'logpz: ', logpz
+            print 'ztotsum: ', ztotsum
+            print 'weights: ', weights
+            
             '''
         
     